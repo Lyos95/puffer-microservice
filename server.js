a3db959e7e80c61a108688ebc06f5aab26024768
unknown <lyosprogramming@gmail.com>
2024-10-07 00:11:38 +0200

    Adding cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -12,8 +15,9 @@ app.get('/api/health', (req, res) => {
   res.json({ message: 'Service is up and running!' });
 });
 
+
 app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the microservice!' });
+  res.json({ message: 'Hello from the microservice v2!' });
 });
 
 app.listen(port, () => {
